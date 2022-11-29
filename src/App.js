20d7a0c9b8726ebb819f3cb5db93c0788dc5bdd1
Andrius Alimas <XandrejoX@yahoo.com>
2022-11-29 11:18:20 +0000

    added styling categories and category containers layout its just hard coded layout for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import no_product_image from "./assets/img/no_product_image.png";
+import "./App.css";
+import no_image from "./assets/img/no_image.png";
 const App = () => {
+  const SHOP_NOW = "Shop Now";
   return (
-    <div className="App">
-      <img src={no_product_image} alt="product_image" />
-      <h2>Hello</h2>
+    <div className="categories-categories d-flex justify-content-center">
+      <div className="category-container">
+        <img src={no_image} alt="product_image" className="no-image" />
+        <div className="category-container-body">
+          <h2>HATS</h2>
+          <p>{SHOP_NOW}</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src={no_image} alt="product_image" className="no-image" />
+        <div className="category-container-body">
+          <h2>JACKETS</h2>
+          <p>{SHOP_NOW}</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src={no_image} alt="product_image" className="no-image" />
+        <div className="category-container-body">
+          <h2>SNEAKERS</h2>
+          <p>{SHOP_NOW}</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src={no_image} alt="product_image" className="no-image" />
+        <div className="category-container-body">
+          <h2>WOMANS</h2>
+          <p>{SHOP_NOW}</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src={no_image} alt="product_image" className="no-image" />
+        <div className="category-container-body">
+          <h2>MENS</h2>
+          <p>{SHOP_NOW}</p>
+        </div>
+      </div>
     </div>
   );
 };
