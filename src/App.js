ba9cf5ab80b2d034f39b0800b78993cb7c459e49
Andrius Alimas <XandrejoX@yahoo.com>
2022-11-29 16:05:06 +0000

    converted from simply hard coded to map arrays hard coded better version

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import "./App.css";
 import no_image from "./assets/img/no_image.png";
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "HATS",
+    },
+    {
+      id: 2,
+      title: "JACKETS",
+    },
+    {
+      id: 3,
+      title: "SNEAKERS",
+    },
+    {
+      id: 4,
+      title: "WOMANS",
+    },
+    {
+      id: 5,
+      title: "MANS",
+    },
+  ];
   const SHOP_NOW = "Shop Now";
   return (
     <div className="categories-categories d-flex justify-content-center">
-      <div className="category-container">
-        <img src={no_image} alt="product_image" className="no-image" />
-        <div className="category-container-body">
-          <h2>HATS</h2>
-          <p>{SHOP_NOW}</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <img src={no_image} alt="product_image" className="no-image" />
-        <div className="category-container-body">
-          <h2>JACKETS</h2>
-          <p>{SHOP_NOW}</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <img src={no_image} alt="product_image" className="no-image" />
-        <div className="category-container-body">
-          <h2>SNEAKERS</h2>
-          <p>{SHOP_NOW}</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <img src={no_image} alt="product_image" className="no-image" />
-        <div className="category-container-body">
-          <h2>WOMANS</h2>
-          <p>{SHOP_NOW}</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <img src={no_image} alt="product_image" className="no-image" />
-        <div className="category-container-body">
-          <h2>MENS</h2>
-          <p>{SHOP_NOW}</p>
-        </div>
-      </div>
+      {categories.map(({ title }) => {
+        return (
+          <div className="category-container">
+            <img src={no_image} alt="product_image" className="no-image" />
+            <div className="category-container-body">
+              <h2>{title}</h2>
+              <p>{SHOP_NOW}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
