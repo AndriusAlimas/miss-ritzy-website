f72620463bcb64a3ba9c4992bf7f5a9fa80b3172
Andrius Alimas <XandrejoX@yahoo.com>
2022-11-29 10:52:27 +0000

    added new logo and no product image just display its working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import no_product_image from "./assets/img/no_product_image.png";
 const App = () => {
-  <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={no_product_image} alt="product_image" />
+      <h2>Hello</h2>
+    </div>
+  );
 };
 
 export default App;
