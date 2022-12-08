fe36f3bc36a5289ad57653419f4b7c915595710d
Andrius Alimas <XandrejoX@yahoo.com>
2022-12-08 15:14:33 +0000

    added navbar and logo with some styling

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Logo } from "../../../assets/svg/Logo.svg";
+import "./navigation.styles.scss";
 const Navigation = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo-container" to={"/"}>
-          <div>Logo</div>
+          <Logo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to={"/shop"}>
