491265016554eabaf0aedce09ad7d2a93d66cc7d
Andrius Alimas <XandrejoX@yahoo.com>
2022-12-13 13:46:37 +0000

    Added signup component form just bare bones

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,12 @@
 import { logGoogleUser } from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Log In With Google</button>
+      <SignUpForm />
     </div>
   );
 };
