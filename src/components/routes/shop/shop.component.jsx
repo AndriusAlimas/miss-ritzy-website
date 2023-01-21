4cabe8a4cae78b3de1bfc92bd0b33203b720791e
Andrius Alimas <XandrejoX@yahoo.com>
2023-01-21 22:18:44 +0000

    created CategoryPreview component with styles. This component will only show 4 products in a row for specific category in shop page

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -3,25 +3,21 @@ import { Fragment, useContext } from "react";
 // Context:
 import { CategoriesContext } from "../../../contexts/categories.context";
 // Components:
-import ProductCard from "../../product-card/product-card.component";
+import CategoryPreview from "../../category-preview/category-preview.component";
 // Styles:
 import "./shop.styles.scss";
 
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 export default Shop;
