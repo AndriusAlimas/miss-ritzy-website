48635babada8c02da966a709e566224e27c5653f
Andrius Alimas <XandrejoX@yahoo.com>
2023-03-04 10:24:02 +0000

    Create category.reducer.js
    created reducer for category

---
+++ src/store/categories/category.reducer.js
@@ -0,0 +1,24 @@
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+const { SET_CATEGORIES_MAP } = CATEGORIES_ACTION_TYPES;
+
+// Initial State for Reducer:
+const CATEGORIES_INITIAL_STATE = {
+  categoriesMap: {},
+};
+
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {}
+) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_CATEGORIES_MAP:
+      return {
+        ...state,
+        categoriesMap: payload,
+      };
+    default:
+      return state;
+  }
+};
