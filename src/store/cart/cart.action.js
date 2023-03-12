19e584d09322854ec6bb54394fa31dc52ca71ca2
Andrius Alimas <XandrejoX@yahoo.com>
2023-03-12 13:32:24 +0000

    setup structure and cart reducer

---
+++ src/store/cart/cart.action.js
@@ -0,0 +1,8 @@
+import { createAction } from "../../utils/reducer/reducer.util";
+import CART_ACTION_TYPES from "./cart.types";
+
+// export const setIsCartOpen = (bool) =>
+//   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool);
+
+// export const setCartItems = (cart) =>
+//   createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cart);
---
+++ src/store/cart/cart.reducer.js
@@ -0,0 +1,28 @@
+import CART_ACTION_TYPES from "./cart.types";
+const { SET_IS_CART_OPEN, SET_CART_ITEMS } = CART_ACTION_TYPES;
+
+// Initial State For Reducer:
+export const CART_INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    case SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    default:
+      return state;
+  }
+};
