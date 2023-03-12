aec8f3a147195ecd118108fc48d8628e8fe3c091
Andrius Alimas <XandrejoX@yahoo.com>
2023-03-12 18:04:36 +0000

    Update cart.action.js
    migrated action and method utilities from the context

--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,8 +1,22 @@
 import { createAction } from "../../utils/reducer/reducer.util";
+import { addOrRemoveCartItem } from "../../utils/cartUpdateHelper";
 import CART_ACTION_TYPES from "./cart.types";
 
-// export const setIsCartOpen = (bool) =>
-//   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool);
+const { SET_IS_CART_OPEN, SET_CART_ITEMS } = CART_ACTION_TYPES;
+export const setIsCartOpen = (bool) => createAction(SET_IS_CART_OPEN, bool);
 
-// export const setCartItems = (cart) =>
-//   createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cart);
+export const setCartItems = (cart) =>
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cart);
+
+export const addItemToCart = (cartItems, product) => {
+  const newCartItems = addOrRemoveCartItem(cartItems, product, "add");
+  return createAction(SET_CART_ITEMS, newCartItems);
+};
+export const removeItemFromCart = (cartItems, product) => {
+  const newCartItems = addOrRemoveCartItem(cartItems, product, "remove");
+  return createAction(SET_CART_ITEMS, newCartItems);
+};
+export const clearItemFromCart = (cartItems, product) => {
+  const newCartItems = addOrRemoveCartItem(cartItems, product, "remove", true);
+  return createAction(SET_CART_ITEMS, newCartItems);
+};
