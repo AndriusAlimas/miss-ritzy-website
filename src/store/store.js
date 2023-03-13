00b6a84612387fc55a74328423ef92c0a2a84a7a
Andrius Alimas <XandrejoX@yahoo.com>
2023-03-13 14:13:36 +0000

    Update store.js
    reactDev tools included

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
@@ -12,22 +12,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next();
-  }
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-  next(action);
-  console.log("next state: ", store.getState());
-};
-
-const middleWares = [loggerMiddleware];
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
