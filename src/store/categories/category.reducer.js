357e9c9b60f69289cc887c5548982fe99dc3fd30
Andrius Alimas <XandrejoX@yahoo.com>
2023-03-25 11:07:18 +0000

    Update category.reducer.js
    update reducer matching redux thunk types

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,9 +1,15 @@
 import CATEGORIES_ACTION_TYPES from "./category.types";
-const { SET_CATEGORIES } = CATEGORIES_ACTION_TYPES;
+const {
+  FETCH_CATEGORIES_START,
+  FETCH_CATEGORIES_SUCCESS,
+  FETCH_CATEGORIES_FAILED,
+} = CATEGORIES_ACTION_TYPES;
 
 // Initial State for Reducer:
 const CATEGORIES_INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReducer = (
@@ -13,9 +19,21 @@ export const categoriesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case SET_CATEGORIES:
+    case FETCH_CATEGORIES_START:
       return {
         ...state,
+        isLoading: true,
+      };
+    case FETCH_CATEGORIES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload,
+      };
+    case FETCH_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
         categories: payload,
       };
     default:
